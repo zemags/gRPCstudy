a80cbea374bb6642885f0e80a82809d4673ac811
zemags <azmt2008@gmail.com>
2021-02-10 19:41:20 +0500

    Refactor

--- blog/server/server.go
+++ blog/server/server.go
@@ -7,38 +7,40 @@ import (
 	"net"
 	"os"
 	"os/signal"
-	"time"
 
 	"github.com/zemags/gRPSstudy/blog/pb"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
 	"google.golang.org/grpc"
 )
 
-var collection *mongo.Collection
+// ServerOpts Define server options
+// include server and db options
+type ServerOpts struct {
+	IPPort   string
+	MongoURL string
+}
 
 func main() {
 	// if code crashed get filename and line number of code
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
-
 	fmt.Println("Starting BlogService")
 
-	// connect to mongo db
-	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
-	if err != nil {
-		log.Fatal(err)
+	servOpts := &ServerOpts{
+		IPPort:   "localhost:50051",
+		MongoURL: "mongodb://localhost:27017",
 	}
-	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
-	defer cancel()
-	err = client.Connect(ctx)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer client.Disconnect(ctx)
-	// if db doesnt exist will be created, Collection - like table in psql
-	collection = client.Database("mydb").Collection("blog")
 
-	lsn, lsnErr := net.Listen("tcp", "localhost:50051")
+	client, err := createMongoClient(servOpts.MongoURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Disconnect(context.Background())
+	mongoParams := MongoParams{
+		DBName:     "mydb",
+		Collection: "blog",
+	}
+	createMongoServer(client, mongoParams)
+
+	lsn, lsnErr := net.Listen("tcp", servOpts.IPPort)
 	if err != nil {
 		log.Fatalf("Failed to listen %v", lsnErr)
 	}
--- blog/server/service.go
+++ blog/server/service.go
@@ -2,10 +2,15 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
+	"log"
+	"time"
 
 	"github.com/zemags/gRPSstudy/blog/pb"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	driver "go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
@@ -15,11 +20,6 @@ type Service struct {
 	pb.UnimplementedBlogServiceServer
 }
 
-// NewService make new empty Service
-func NewService() *Service {
-	return &Service{}
-}
-
 type blogItem struct {
 	ID       primitive.ObjectID `bson:"_id,omitempty"` // autoincrement
 	AuthorID string             `bson:"author_id"`
@@ -27,8 +27,58 @@ type blogItem struct {
 	Content  string             `bson:"content"`
 }
 
+// Mongo collect all options to work with mongo
+type Mongo struct {
+	*driver.Client
+	MongoParams
+}
+
+// MongoParams collection given dbname and collection
+type MongoParams struct {
+	DBName     string
+	Collection string
+}
+
+// NewService make new empty Service
+func NewService() *Service {
+	return &Service{}
+}
+
+func createMongoClient(mongoURL string) (*driver.Client, error) {
+	log.Printf("Create mongo client for %s", mongoURL)
+	if mongoURL == "" {
+		log.Fatalf("Cannot create client for Mongo db, no URL")
+		return nil, errors.New("no url for Mongo client")
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	client, err := driver.Connect(ctx, options.Client().ApplyURI(mongoURL))
+	if err != nil {
+		log.Fatalf("Cannot connect to %v", err)
+		return nil, errors.New("cannot connect to MongoDB")
+	}
+	return client, nil
+}
+
+func createMongoServer(client *driver.Client, mp MongoParams) *Mongo {
+	log.Printf("Create new mongo server")
+	return &Mongo{
+		Client:      client,
+		MongoParams: mp,
+	}
+}
+
+// GetCollection impliment collection and return error
+func (m *Mongo) getMongoCollection() *driver.Collection {
+	log.Printf("Create mongo collection %v for db %v", m.Collection, m.DBName)
+	coll := m.Database(m.DBName).Collection(m.Collection)
+	return coll
+}
+
 // CreateBlog insert value for blog to db
-func (*Service) CreateBlog(ctx context.Context, req *pb.CreateBlogRequest) (*pb.CreateBlogResponse, error) {
+func (m *Mongo) CreateBlog(ctx context.Context, req *pb.CreateBlogRequest) (*pb.CreateBlogResponse, error) {
 	blog := req.GetBlog()
 
 	data := blogItem{
@@ -37,7 +87,8 @@ func (*Service) CreateBlog(ctx context.Context, req *pb.CreateBlogRequest) (*pb.
 		Content:  blog.GetContent(),
 	}
 
-	res, err := collection.InsertOne(context.Background(), data)
+	coll := m.Database(m.DBName).Collection(m.Collection)
+	res, err := coll.InsertOne(context.Background(), data)
 	if err != nil {
 		return nil, status.Errorf(
 			codes.Internal, // Internal db error
